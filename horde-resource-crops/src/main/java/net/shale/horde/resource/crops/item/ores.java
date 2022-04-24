930bf5bf2587c8e120b221ce1bf36f675196d0d4
Lucy <gakupo.gumi@gmail.com>
2022-04-24 20:31:22 +1200

    Remapping textures

--- horde-resource-crops/src/main/java/net/shale/horde/resource/crops/item/ores.java
+++ horde-resource-crops/src/main/java/net/shale/horde/resource/crops/item/ores.java
@@ -8,17 +8,17 @@ import net.minecraft.util.registry.Registry;
 import net.shale.horde.resource.crops.Main;
 
 public class ores {
-    public static final Item DRAGONSTONE_OVERWORLD = registerItem("dragonstone_overworld", new Item(new FabricItemSettings()
+    public static final Item DRAGONSTONE_OVERWORLD = registerItem("ores/dragonstone_overworld", new Item(new FabricItemSettings()
             .group(ItemGroup.MATERIALS)));
-    public static final Item RAW_DRAGONSTONE_OVERWORLD = registerItem("raw_dragonstone_overworld", new Item(new FabricItemSettings()
+    public static final Item RAW_DRAGONSTONE_OVERWORLD = registerItem("ores/raw_dragonstone_overworld", new Item(new FabricItemSettings()
             .group(ItemGroup.MATERIALS)));
-    public static final Item DRAGONSTONE_NETHER = registerItem("dragonstone_nether", new Item(new FabricItemSettings()
+    public static final Item DRAGONSTONE_NETHER = registerItem("ores/dragonstone_nether", new Item(new FabricItemSettings()
             .group(ItemGroup.MATERIALS)));
-    public static final Item RAW_DRAGONSTONE_NETHER = registerItem("raw_dragonstone_nether", new Item(new FabricItemSettings()
+    public static final Item RAW_DRAGONSTONE_NETHER = registerItem("ores/raw_dragonstone_nether", new Item(new FabricItemSettings()
             .group(ItemGroup.MATERIALS)));
-    public static final Item DRAGONSTONE_END = registerItem("dragonstone_end", new Item(new FabricItemSettings()
+    public static final Item DRAGONSTONE_END = registerItem("ores/dragonstone_end", new Item(new FabricItemSettings()
             .group(ItemGroup.MATERIALS)));
-    public static final Item RAW_DRAGONSTONE_END = registerItem("raw_dragonstone_end", new Item(new FabricItemSettings()
+    public static final Item RAW_DRAGONSTONE_END = registerItem("ores/raw_dragonstone_end", new Item(new FabricItemSettings()
             .group(ItemGroup.MATERIALS)));
 
     private static Item registerItem(String name, Item item) {
