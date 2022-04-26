1372f465aaff717ef9ace221d697e9e335003e40
Lucy <gakupo.gumi@gmail.com>
2022-04-27 09:19:55 +1200

    Updating crop code

--- horde-resource-crops/src/main/java/net/shale/horde/resource/crops/Main.java
+++ horde-resource-crops/src/main/java/net/shale/horde/resource/crops/Main.java
@@ -5,11 +5,13 @@ import net.minecraft.util.Identifier;
 import net.shale.horde.resource.crops.block.ores.blocks_ores;
 import net.shale.horde.resource.crops.block.vanilla.T0__crops;
 import net.shale.horde.resource.crops.block.vanilla.T1__crops;
+import net.shale.horde.resource.crops.block.vanilla.T2__crops;
 import net.shale.horde.resource.crops.item.items_ores;
 import net.shale.horde.resource.crops.item.other.gemstones;
 import net.shale.horde.resource.crops.item.other.shards;
 import net.shale.horde.resource.crops.item.vanilla.T0__seeds;
 import net.shale.horde.resource.crops.item.vanilla.T1__seeds;
+import net.shale.horde.resource.crops.item.vanilla.T2__seeds;
 import net.shale.horde.resource.crops.item.vanilla.mixes;
 
 public class Main implements ModInitializer {
@@ -30,5 +32,7 @@ public class Main implements ModInitializer {
         T0__seeds.registerModItems();
         T1__crops.registerBlock();
         T1__seeds.registerModItems();
+        T2__crops.registerBlock();
+        T2__seeds.registerModItems();
     }
 }
