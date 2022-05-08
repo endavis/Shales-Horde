532e720db1b278b32063bba725d14bcda1c34359
Lucy <gakupo.gumi@gmail.com>
2022-05-09 08:24:01 +1200

    fixed mixin

---
+++ test/src/main/java/test/test/client/TestClient.java
@@ -0,0 +1,13 @@
+package test.test.client;
+
+import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
+
+@Environment(EnvType.CLIENT)
+public class TestClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
