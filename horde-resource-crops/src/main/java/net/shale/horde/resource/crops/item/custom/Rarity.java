3d90b0e6719af79c2bf9dcc0131a038308c88c72
Lucy <gakupo.gumi@gmail.com>
2022-05-10 19:05:18 +1200

    tooltips

---
+++ horde-resource-crops/src/main/java/net/shale/horde/resource/crops/item/custom/Rarity.java
@@ -0,0 +1,15 @@
+package net.shale.horde.resource.crops.item.custom;
+
+//public enum Rarity {
+//    COMMON(Formatting.WHITE),
+//    UNCOMMON(Formatting.YELLOW),
+//    RARE(Formatting.AQUA),
+//    EPIC(Formatting.LIGHT_PURPLE),
+//    ELEMENTAL(Formatting.RED);
+//
+//    public final Formatting formatting;
+//
+//    Rarity(Formatting formatting) {
+//        this.formatting = formatting;
+//    }
+//}
