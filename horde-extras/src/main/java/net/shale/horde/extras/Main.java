1b727d199b5e073bf7dfa7ff3d609bdb7513f741
Lucy <gakupo.gumi@gmail.com>
2022-05-16 16:47:50 +1200

    extra init

---
+++ horde-extras/src/main/java/net/shale/horde/extras/Main.java
@@ -0,0 +1,17 @@
+package net.shale.horde.extras;
+
+import net.fabricmc.api.ModInitializer;
+import net.minecraft.util.Identifier;
+
+public class Main implements ModInitializer {
+    public static final String ID = "horde-extras";
+
+    public static Identifier id(String path) {
+        return new Identifier(ID, path);
+    }
+
+    @Override
+    public void onInitialize() {
+        
+    }
+}
