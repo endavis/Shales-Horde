2fdd2d60e25606c25a52891dec468dc10c2c650a
Lucy <gakupo.gumi@gmail.com>
2022-05-16 19:41:48 +1200

    deco init

--- horde-deco/src/main/java/Main.java
+++ horde-deco/src/main/java/Main.java
@@ -1,10 +1,8 @@
-package net.shale.horde.extras;
-
 import net.fabricmc.api.ModInitializer;
 import net.minecraft.util.Identifier;
 
 public class Main implements ModInitializer {
-    public static final String ID = "horde-extras";
+    public static final String ID = "horde-deco";
 
     public static Identifier id(String path) {
         return new Identifier(ID, path);
