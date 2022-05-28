bc4d6fc8450152bdea8177a4f7f5dbe4c89ad40e
Lucy <gakupo.gumi@gmail.com>
2022-05-29 10:05:27 +1200

    Husk, cave spider and stray drop sand, cobwebs and ice respectfully

--- horde-extras/src/main/java/net/shale/horde/extras/Main.java
+++ horde-extras/src/main/java/net/shale/horde/extras/Main.java
@@ -2,6 +2,7 @@ package net.shale.horde.extras;
 
 import net.fabricmc.api.ModInitializer;
 import net.minecraft.util.Identifier;
+import net.shale.horde.extras.util.modified_loot_tables;
 
 public class Main implements ModInitializer {
     public static final String ID = "horde-extras";
@@ -12,6 +13,6 @@ public class Main implements ModInitializer {
 
     @Override
     public void onInitialize() {
-        
+        modified_loot_tables.modifyLootTables();
     }
 }
---
+++ horde-extras/src/main/java/net/shale/horde/extras/util/modified_loot_tables.java
@@ -0,0 +1,51 @@
+package net.shale.horde.extras.util;
+
+import net.fabricmc.fabric.api.loot.v1.FabricLootPoolBuilder;
+import net.fabricmc.fabric.api.loot.v1.event.LootTableLoadingCallback;
+import net.minecraft.item.Items;
+import net.minecraft.loot.condition.RandomChanceLootCondition;
+import net.minecraft.loot.entry.ItemEntry;
+import net.minecraft.loot.function.SetCountLootFunction;
+import net.minecraft.loot.provider.number.ConstantLootNumberProvider;
+import net.minecraft.loot.provider.number.UniformLootNumberProvider;
+import net.minecraft.util.Identifier;
+
+public class modified_loot_tables {
+    private static final Identifier CAVE_SPIDER = new Identifier("minecraft", "entities/cave_spider");
+    private static final Identifier HUSK = new Identifier("minecraft", "entities/husk");
+    private static final Identifier STRAY = new Identifier("minecraft", "entities/stray");
+    private static final Identifier WITCH = new Identifier("minecraft", "entities/witch");
+
+    public static void modifyLootTables() {
+        LootTableLoadingCallback.EVENT.register(((resourceManager, manager, id, supplier, setter) -> {
+            if (CAVE_SPIDER.equals(id)) {
+                FabricLootPoolBuilder poolBuilder = FabricLootPoolBuilder.builder()
+                        .rolls(ConstantLootNumberProvider.create(1))
+                        .conditionally(RandomChanceLootCondition.builder(0.5f))
+                        .with(ItemEntry.builder(Items.COBWEB))
+                        .withFunction(SetCountLootFunction.builder(UniformLootNumberProvider.create(1.0f, 1.0f)).build());
+                supplier.withPool(poolBuilder.build());
+            }
+        }));
+        LootTableLoadingCallback.EVENT.register(((resourceManager, manager, id, supplier, setter) -> {
+            if (HUSK.equals(id)) {
+                FabricLootPoolBuilder poolBuilder = FabricLootPoolBuilder.builder()
+                        .rolls(ConstantLootNumberProvider.create(1))
+                        .conditionally(RandomChanceLootCondition.builder(0.5f))
+                        .with(ItemEntry.builder(Items.SAND))
+                        .withFunction(SetCountLootFunction.builder(UniformLootNumberProvider.create(1.0f, 1.0f)).build());
+                supplier.withPool(poolBuilder.build());
+            }
+        }));
+        LootTableLoadingCallback.EVENT.register(((resourceManager, manager, id, supplier, setter) -> {
+            if (STRAY.equals(id)) {
+                FabricLootPoolBuilder poolBuilder = FabricLootPoolBuilder.builder()
+                        .rolls(ConstantLootNumberProvider.create(1))
+                        .conditionally(RandomChanceLootCondition.builder(0.5f))
+                        .with(ItemEntry.builder(Items.ICE))
+                        .withFunction(SetCountLootFunction.builder(UniformLootNumberProvider.create(1.0f, 1.0f)).build());
+                supplier.withPool(poolBuilder.build());
+            }
+        }));
+    }
+}
