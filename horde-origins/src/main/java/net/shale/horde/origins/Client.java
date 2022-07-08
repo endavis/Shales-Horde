89888dbf7bd090c98773243b47fe86400583b807
Lucy <gakupo.gumi@gmail.com>
2022-07-08 15:19:47 +1200

    still setting up

---
+++ horde-origins/src/main/java/net/shale/horde/origins/Client.java
@@ -0,0 +1,10 @@
+package net.shale.horde.origins;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class Client implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ horde-origins/src/main/java/net/shale/horde/origins/Main.java
@@ -0,0 +1,17 @@
+package net.shale.horde.origins;
+
+import net.fabricmc.api.ModInitializer;
+import net.minecraft.util.Identifier;
+
+public class Main implements ModInitializer {
+    public static final String ID = "horde-origins";
+
+
+    public static Identifier id(String path) {
+        return new Identifier(ID, path);
+    }
+
+    @Override
+    public void onInitialize() {
+    }
+}
