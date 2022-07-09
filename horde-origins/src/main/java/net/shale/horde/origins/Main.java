becb36aad9c0dbaa49535bc1c7a67e16313513c7
Lucy <gakupo.gumi@gmail.com>
2022-07-10 00:31:26 +1200

    Init

--- horde-origins/src/main/java/net/shale/horde/origins/Main.java
+++ horde-origins/src/main/java/net/shale/horde/origins/Main.java
@@ -6,12 +6,12 @@ import net.minecraft.util.Identifier;
 public class Main implements ModInitializer {
     public static final String ID = "horde-origins";
 
-
     public static Identifier id(String path) {
         return new Identifier(ID, path);
     }
 
     @Override
     public void onInitialize() {
+
     }
 }
