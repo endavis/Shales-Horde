5de13828af0df0827d332952076e9892db20d442
Lucy <gakupo.gumi@gmail.com>
2022-07-20 19:46:40 +1200

    boosted

--- horde-armoury/src/main/java/net/shale/horde/armoury/items/shields.java
+++ horde-armoury/src/main/java/net/shale/horde/armoury/items/shields.java
@@ -12,13 +12,13 @@ import net.shale.horde.armoury.Main;
 
 public class shields {
     public static final Item IRON_SHIELD = registerItem("iron_shield", new FabricShieldItem(new FabricItemSettings()
-            .maxDamage(2500).group(ItemGroup.COMBAT),10, 13, Items.IRON_INGOT ));
+            .maxDamage(800).group(ItemGroup.COMBAT),100, 3, Items.IRON_INGOT ));
     public static final Item GOLD_SHIELD = registerItem("gold_shield", new FabricShieldItem(new FabricItemSettings()
-            .maxDamage(2500).group(ItemGroup.COMBAT),10, 13, Items.GOLD_INGOT ));
+            .maxDamage(500).group(ItemGroup.COMBAT),100, 10, Items.GOLD_INGOT ));
     public static final Item DIAMOND_SHIELD = registerItem("diamond_shield", new FabricShieldItem(new FabricItemSettings()
-            .maxDamage(2500).group(ItemGroup.COMBAT),10, 13, Items.DIAMOND ));
+            .maxDamage(1500).group(ItemGroup.COMBAT),100, 5, Items.DIAMOND ));
     public static final Item NETHERITE_SHIELD = registerItem("netherite_shield", new FabricShieldItem(new FabricItemSettings()
-            .maxDamage(2500).group(ItemGroup.COMBAT),10, 13, Items.NETHERITE_INGOT ));
+            .maxDamage(2500).group(ItemGroup.COMBAT),100, 8, Items.NETHERITE_INGOT ));
 
     private static Item registerItem(String name, Item item) {
         return Registry.register(Registry.ITEM, new Identifier(Main.ID, name), item);
